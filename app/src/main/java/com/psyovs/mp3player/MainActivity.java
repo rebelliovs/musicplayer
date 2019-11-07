8f527e7283f72460d17f305151c72b4dba976915
Octavian V. Salagean <vlad.octav@outlook.com>
2019-11-07 19:37:31 +0000

    all functional, progress needs to be added

--- app/src/main/java/com/psyovs/mp3player/MainActivity.java
+++ app/src/main/java/com/psyovs/mp3player/MainActivity.java
@@ -13,15 +13,19 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ListView;
 import android.widget.ToggleButton;
+
+import com.example.pszmdf.mp3test.MP3Player;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
-    ToggleButton playPause;
+    //ToggleButton playPause;
+    Button play, pause, stop, refresh;
     private MusicService.ServiceBinder musicBinder = null;
 
     @Override
@@ -43,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
                 File selectedFromList =(File) (lv.getItemAtPosition(myItemInt));
                 Log.d("g53mdp", selectedFromList.getAbsolutePath());
                 // do something with selectedFromList...
+                musicBinder.getSong(selectedFromList.getAbsolutePath());
+                //updateGraphic();
             }
         });
 
@@ -56,28 +62,85 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        if(musicBinder.state() == MP3Player.MP3PlayerState.PLAYING) {
+            musicBinder.notification();
+        } else {
+            musicBinder.stopNotifs();
+            if(serviceConnection!=null) {
+                unbindService(serviceConnection);
+                serviceConnection = null;
+            }
+        }
     }
 
     public void onToggle (View v) {
 
-        playPause = (ToggleButton) findViewById(R.id.buttonState);
+        play = (Button) findViewById(R.id.buttonPlay);
+        pause = (Button) findViewById(R.id.buttonPause);
+        stop = (Button) findViewById(R.id.buttonStop);
+        refresh = (Button) findViewById(R.id.buttonRefresh);
 
-        playPause.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        play.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
+            public void onClick(View v) {
+                musicBinder.play();
+            }
+        });
 
-                } else {
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                musicBinder.pause();
+            }
+        });
 
-                }
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                musicBinder.stop();
+            }
+        });
+
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
 
     }
 
-    public void onStop (View v) {
 
-    }
+    // have to change the toggle to 2 buttons because it wont change the state correctly if a song is selected first...
+//    public void onToggle (View v) {
+//
+//        playPause = (ToggleButton) findViewById(R.id.buttonState);
+//
+//        playPause.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+//            @Override
+//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+//                if (isChecked) {
+//                    musicBinder.play();
+//                } else {
+//                    musicBinder.pause();
+//                }
+//            }
+//        });
+//
+//    }
+
+    // added to the new onToggle instead
+//    public void onStop (View v) {
+//        musicBinder.stop();
+//    }
+
+//    app crashes if this is called and song is selected first.
+//    public void updateGraphic() {
+//        if(musicBinder.state() == MP3Player.MP3PlayerState.PLAYING) {
+//            playPause.setChecked(true);
+//        }
+//    }
 
     private ServiceConnection serviceConnection = new ServiceConnection()
     {
--- app/src/main/java/com/psyovs/mp3player/MusicService.java
+++ app/src/main/java/com/psyovs/mp3player/MusicService.java
@@ -1,15 +1,22 @@
 package com.psyovs.mp3player;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
+import android.os.Build;
 import android.os.IBinder;
+
+import androidx.core.app.NotificationCompat;
 
 import com.example.pszmdf.mp3test.MP3Player;
 
 public class MusicService extends Service {
 
     MP3Player mp3 = new MP3Player();
+
     private final IBinder binder = new ServiceBinder();
 
     private final String CHANNEL_ID = "100";
@@ -24,7 +31,85 @@ public class MusicService extends Service {
         return binder;
     }
 
+    private void notification() {
+        NotificationManager notificationManager = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Music";
+            String description = "Song is playing";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name,
+                    importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            notificationManager.createNotificationChannel(channel);
+
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this,
+                    CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle("Music..")
+                    .setContentText("..is playing")
+                    .setContentIntent(pendingIntent)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            startForeground(NOTIFICATION_ID, mBuilder.build());
+        }
+    }
+
+    private void stopNotifs() {
+        stopForeground(true);
+        stopSelf();
+    }
+
     public class ServiceBinder extends Binder {
 
+        // these 2 need to be outside and called here again..
+        // tried having the stopnotifs in the stop button, but not working how i planned
+        // so i'll have them separate.
+        void notification() {MusicService.this.notification();}
+        void stopNotifs() {MusicService.this.stopNotifs();}
+
+        public void play() {
+            mp3.play();
+        }
+
+        public void pause() {
+            mp3.pause();
+        }
+
+        public void stop() {
+            mp3.stop();
+        }
+
+        public void getSong(String selected) {
+            //check if a song is already playing, if so, stop it
+            if(mp3.getState() == MP3Player.MP3PlayerState.PLAYING) {
+                stop();
+            }
+
+            mp3.load(selected);
+        }
+
+        MP3Player.MP3PlayerState state() {
+            return mp3.getState();
+        }
+
+        int duration() {
+            return mp3.getDuration()/1000;
+        }
+
+        int progress() {
+            return mp3.getProgress()/1000;
+        }
+
     }
+
 }
