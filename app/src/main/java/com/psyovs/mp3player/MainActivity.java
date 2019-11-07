9a0c594ac809d6ff7cae68262c771179232a8dd0
Octavian V. Salagean <vlad.octav@outlook.com>
2019-11-07 16:01:39 +0000

    service added, XML modified

--- app/src/main/java/com/psyovs/mp3player/MainActivity.java
+++ app/src/main/java/com/psyovs/mp3player/MainActivity.java
@@ -3,6 +3,14 @@ package com.psyovs.mp3player;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ListView lv = (ListView) findViewById(R.id.songs);
+        File musicDir = new File(
+                Environment.getExternalStorageDirectory().getPath()+ "/Music/");
+        File list[] = musicDir.listFiles();
+        lv.setAdapter(new ArrayAdapter<File>(this,
+                android.R.layout.simple_list_item_1, list));
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> myAdapter,
+                                    View myView,
+                                    int myItemInt,
+                                    long mylng) {
+                File selectedFromList =(File) (lv.getItemAtPosition(myItemInt));
+                Log.d("g53mdp", selectedFromList.getAbsolutePath());
+                // do something with selectedFromList...
+            }
+        });
+
     }
 }
